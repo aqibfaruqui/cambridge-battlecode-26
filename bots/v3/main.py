35cd3ea4ea4de5d82fc06ef94410470301e34d6d
aqibfaruqui <aqibfaruqui@gmail.com>
2026-03-21 00:49:47 +0000

    init(bots/v3): Copy v2 to start v3

---
+++ bots/v3/main.py
@@ -0,0 +1,33 @@
+""" 
+V2 Bot:
+- Core: Spawn 8 bots on random adjacent tiles
+- Builder Bot: Designate half to harvest ores and build conveyer belt back to our core
+               Designate half to exploring and building turrets at enemy core
+
+Test code runs with: cambc run v1 v2
+"""
+
+from cambc import Controller, EntityType
+from core.core import Core
+from builders.builder import Builder
+from turrets.turret import Turret
+
+class Player:
+    def __init__(self):
+        self.unit = None
+
+    def _init_unit(self, c: Controller):
+        unit_etype = c.get_entity_type()
+        match unit_etype:
+            case EntityType.CORE: 
+                return Core()
+            case EntityType.BUILDER_BOT:
+                return Builder()
+            case _:
+                return Turret(unit_etype)
+
+    def run(self, c: Controller):
+        if self.unit is None:
+            self.unit = self._init_unit(c)
+
+        self.unit.run(c)

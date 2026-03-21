2498948566411759de1a4d92c322a5ae796e0105
Izhaan Salam <izhaansalam@gmail.com>
2026-03-21 02:24:12 +0000

    feat(v4): bug-nav function

--- bots/v4/utils/movement.py
+++ bots/v4/utils/movement.py
@@ -1,5 +1,5 @@
 import random
-from cambc import Direction, Position
+from cambc import Controller, Direction, Position
 
 DIRECTIONS_4 = [
     Direction.NORTH,
@@ -83,3 +83,37 @@ def split_diagonal(current: Position, target: Position):
             return (Direction.NORTH, Direction.WEST)
         case _:
             return (None, None)
+
+def bug_nav(c: Controller, current: Position, target: Position, follow_dir: Direction = None):
+    # current_dir is the direction the bot is currently following along wall
+    # Returns the move direction and the direction the bot should be following along wall
+    preferred_move = get_direction_4(current, target) # only move in cardinal directions as conveyors need to be placed
+    
+    if follow_dir is None:
+        if c.can_move(preferred_move):
+            return preferred_move, follow_dir
+        
+        # Hit a wall, pick side closer to target
+
+        left_side = preferred_move.rotate_left().rotate_left() # 90 degrees left
+        right_side = preferred_move.rotate_right().rotate_right() # 90 degrees right
+
+        if _manhattan(current.add(left_side), target) < _manhattan(current.add(right_side), target):
+            follow_dir = left_side
+        else:
+            follow_dir = right_side
+    
+    # Exit wall-hugging as soon as direct path clears (keep follow_dir to prevent oscillation)
+    if c.can_move(preferred_move):
+        return preferred_move, follow_dir
+    
+    for d in (
+        follow_dir,
+        follow_dir.rotate_left().rotate_left(),
+        follow_dir.rotate_right().rotate_right(),
+        follow_dir.rotate_right().rotate_right().rotate_right().rotate_right(),
+    ):
+        if c.can_move(d):
+            return d, d
+    
+    return None, follow_dir # completely surrounded

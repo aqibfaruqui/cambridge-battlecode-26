d072c2c5c68b8d247a65899f1bf449adc809dd6f
aqibfaruqui <aqibfaruqui@gmail.com>
2026-03-22 16:44:46 +0000

    feat(v5): Upgrade to 6 harvesters, 1 attacker

--- bots/v5/core/core.py
+++ bots/v5/core/core.py
@@ -8,13 +8,13 @@ from utils.movement import (
 class Core:
     def __init__(self):
         self.builders_spawned = 0
-        self.builders_max = 5
+        self.builders_max = 7
 
     def run(self, c: Controller):
         if self.builders_spawned < self.builders_max:
             core_pos = c.get_position()
-            if self.builders_spawned < 4:
-                spawn_dir = DIRECTIONS_4[self.builders_spawned]  # HARVESTER
+            if self.builders_spawned < 6:
+                spawn_dir = DIRECTIONS_4[self.builders_spawned % 4]  # HARVESTER
             else:
                 spawn_dir = direction_to_centre(c, core_pos)  # ATTACKER
                 if spawn_dir in DIRECTIONS_4:

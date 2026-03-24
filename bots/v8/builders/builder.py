0f6e92382e996249ab6237753f8e38d88ae55853
aqibfaruqui <aqibfaruqui@gmail.com>
2026-03-24 03:33:25 +0000

    init(v8): Copy v7 to start v8

---
+++ bots/v8/builders/builder.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from cambc import Controller, EntityType
+from builders.harvester import Harvester
+from builders.attacker import Attacker
+from utils.movement import (
+    DIRECTIONS_4,
+)
+
+
+class BuilderRole(Enum):
+    __slots__ = ()
+
+    HARVESTER = "harvester"
+    ATTACKER = "attacker"
+
+
+class Builder:
+    def __init__(self):
+        self.role = None
+        self.core_pos = None
+
+    def run(self, c: Controller):
+        if self.core_pos is None:
+            for eid in c.get_nearby_buildings():
+                if (
+                    c.get_entity_type(eid) == EntityType.CORE
+                    and c.get_team(eid) == c.get_team()
+                ):
+                    self.core_pos = c.get_position(eid)
+
+        if self.role is None:
+            spawn_dir = self.core_pos.direction_to(c.get_position())
+            if spawn_dir in DIRECTIONS_4:
+                self.role = BuilderRole.HARVESTER
+            else:
+                self.role = BuilderRole.ATTACKER
+
+        match self.role:
+            case BuilderRole.HARVESTER:
+                self.role = Harvester(self.core_pos)
+            case BuilderRole.ATTACKER:
+                self.role = Attacker(self.core_pos)
+
+        # Builder bots can reassign their roles
+        # e.g. Harvester.run() may return Attacker(self.core_pos)
+        new_role = self.role.run(c)
+        if new_role is not None:
+            self.role = new_role
---
+++ bots/v8/utils/board.py
@@ -0,0 +1,104 @@
+from cambc import Controller, EntityType, Environment, Position
+from utils.movement import DIRECTIONS_4, on_map
+
+action_radius = {
+    "bot": 2,
+}
+
+
+def is_wall(c: Controller, pos: Position) -> bool:
+    return c.get_tile_env(pos) == Environment.WALL
+
+
+def is_tile_conveyor(c: Controller, pos: Position) -> bool:
+    id = c.get_tile_building_id(pos)
+    if id is None:
+        return False
+
+    etype = c.get_entity_type(id)
+    return etype == EntityType.CONVEYOR
+
+
+def is_tile_foundry(c: Controller, pos: Position) -> bool:
+    id = c.get_tile_building_id(pos)
+    if id is None:
+        return False
+
+    etype = c.get_entity_type(id)
+    return etype == EntityType.FOUNDRY
+
+
+def is_tile_splitter(c: Controller, pos: Position) -> bool:
+    id = c.get_tile_building_id(pos)
+    if id is None:
+        return False
+
+    etype = c.get_entity_type(id)
+    return etype == EntityType.SPLITTER
+
+
+def replace_with_conveyor(c: Controller, pos: Position, core_pos: Position):
+    if c.can_destroy(pos):
+        c.destroy(pos)
+        conveyor_dir = pos.direction_to(core_pos)
+        if conveyor_dir not in DIRECTIONS_4:
+            conveyor_dir = conveyor_dir.rotate_left()
+        if c.can_build_conveyor(pos, conveyor_dir):
+            c.build_conveyor(pos, conveyor_dir)
+
+
+def is_ore_titanium(c: Controller, pos: Position) -> bool:
+    if not on_map(c, pos):
+        return False
+    env = c.get_tile_env(pos)
+    return env == Environment.ORE_TITANIUM
+
+
+def is_ore_axionite(c: Controller, pos: Position) -> bool:
+    if not on_map(c, pos):
+        return False
+    env = c.get_tile_env(pos)
+    return env == Environment.ORE_AXIONITE
+
+
+def is_ore(c: Controller, pos: Position) -> bool:
+    return is_ore_titanium(c, pos) or is_ore_axionite(c, pos)
+
+
+def nearby_titanium(c: Controller, pos: Position) -> Position:
+    for tile in c.get_nearby_tiles():
+        if not is_ore_titanium(c, tile):
+            continue
+
+        build_id = c.get_tile_building_id(tile)
+        if build_id is None:
+            return tile
+
+        etype = c.get_entity_type(build_id)
+        if etype == EntityType.ROAD and c.can_destroy(tile):
+            return tile
+
+    return None
+
+
+def nearby_ore(c: Controller, pos: Position) -> Position:
+    for tile in c.get_nearby_tiles():
+        if not is_ore(c, tile):
+            continue
+
+        build_id = c.get_tile_building_id(tile)
+        if build_id is None:
+            return tile
+
+        etype = c.get_entity_type(build_id)
+        if etype == EntityType.ROAD and c.can_destroy(tile):
+            return tile
+
+    return None
+
+
+def on_core_border(pos: Position, core_pos: Position):
+    dx = abs(pos.x - core_pos.x)
+    dy = abs(pos.y - core_pos.y)
+
+    return max(dx, dy) == 2 and min(dx, dy) <= 1

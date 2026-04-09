7e29b2a8e2f106bc649a7b6a2a97eb073511855b
Izhaan Salam <izhaansalam@gmail.com>
2026-04-09 16:33:52 +0100

    cleaning up repo

--- battlecode-dashboard/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ReplayProvider } from "@/contexts/replay-context";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ReplayProvider>{children}</ReplayProvider>;
-}

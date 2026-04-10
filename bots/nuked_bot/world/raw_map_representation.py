40b40fb3dfb7c8b5c56432c434902a06012d1c44
Alex Do <alexxdo2004@gmail.com>
2026-04-10 18:39:35 +0100

    feat: make map faster

--- bots/nuked_bot/world/raw_map_representation.py
+++ bots/nuked_bot/world/raw_map_representation.py
@@ -15,38 +15,19 @@ CHAR_MAP = [".", "█", "T", "A", "◎", "◉"]
 
 
 class EnvironmentMap:
-    __slots__ = ("c", "_w", "_h", "_array")
+    __slots__ = ("_w", "_h", "_array")
 
-    def __init__(self, c: Controller):
-        self.c = c
-        self._w = c.get_map_width()
-        self._h = c.get_map_height()
-        self._array = bytearray(self._w * self._h)
-
-    def _idx(self, pos: Position) -> int:
-        return pos.y * self._w + pos.x
-
-    def at(self, pos: Position) -> CustomEnv:
-        return CustomEnv(self._array[self._idx(pos)])
-
-    def set(self, pos: Position, env: CustomEnv) -> None:
-        self._array[self._idx(pos)] = env
+    def __init__(self, w: int, h: int):
+        self._w = w
+        self._h = h
+        self._array = bytearray(w * h)
 
     def at_raw(self, x: int, y: int) -> int:
         return self._array[y * self._w + x]
 
-    def set_raw(self, x: int, y: int, value: int) -> None:
-        self._array[y * self._w + x] = value
+    def set_raw(self, x: int, y: int, v: int) -> None:
+        self._array[y * self._w + x] = v
 
-    @property
-    def width(self) -> int:
-        return self._w
-
-    @property
-    def height(self) -> int:
-        return self._h
-
-    # ---- rendering ----
     def __str__(self) -> str:
         w = self._w
         h = self._h
@@ -56,7 +37,10 @@ class EnvironmentMap:
         rows = []
         for y in range(h):
             base = y * w
-            rows.append("".join(cmap[arr[base + x]] for x in range(w)))
+            row = [""] * w
+            for x in range(w):
+                row[x] = cmap[arr[base + x]]
+            rows.append("".join(row))
         return "\n".join(rows)
 
     def __repr__(self) -> str:

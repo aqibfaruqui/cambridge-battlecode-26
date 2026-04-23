0a78bcd92937b9e5ab60e76ca0da3c0f054ed4cc
Alex Do <alexxdo2004@gmail.com>
2026-04-23 10:10:06 +0100

    feat: broadcast cooldown + no road destruction

--- bots/v11/utils/comms/broadcaster.py
+++ bots/v11/utils/comms/broadcaster.py
@@ -4,27 +4,33 @@ class Broadcaster:
     def __init__(self):
         self.broadcasts: list[int] = []
         self._curr = 0
+        self._cd = 0
+        self._max_pause = 4
 
     def run(self, c: Controller):
-        print(f"[Broadcaster] run broadcasts={self.broadcasts} curr={self._curr}")
         if not self.broadcasts:
+            return
+        
+        if self._cd > 0:
+            self._cd -= 1
             return
 
         for tile in c.get_nearby_tiles(2):
             bid = c.get_tile_building_id(tile)
-            our_road_or_marker = (
+            our_marker = (
                 bid is not None 
-                and c.get_entity_type(bid) in {EntityType.ROAD, EntityType.MARKER}
+                and c.get_entity_type(bid) == EntityType.MARKER
                 and c.get_team(bid) == c.get_team()
                 and c.get_tile_builder_bot_id(tile) is None
             )
 
-            if our_road_or_marker and c.can_destroy(tile):
+            if our_marker and c.can_destroy(tile):
                 c.destroy(tile)
                 
             if c.can_place_marker(tile):
                 c.place_marker(tile, self.broadcasts[self._curr])
                 self._curr += 1
+                self._cd = self._max_pause
                 self._curr %= len(self.broadcasts)
                 break
 

0894fd85bc650b12c51ba5ce9e63b3ccb0c5996a
Alex Do <alexxdo2004@gmail.com>
2026-05-01 07:47:51 +0100

    chore: revert v12

--- bots/v12/builders/axioniter.py
+++
@@ -1,8 +0,0 @@
-from cambc import Position, Controller
-
-class Axioniter:
-    def __init__(self, core_pos: Position):
-        self.core_pos = core_pos
-
-    def run(self, c: Controller):
-        pass
